3d9cd9ad9d6f3387b2e8c3d93bec9c7469b6ffc0
Taani Maama <taani1maama@live.com>
2020-10-25 12:53:23 -0700

    This is the beginning of my project GitHub. I have started with some of the essentials being a HTML structure, a CSS file, and a JS script which I started the pseudocode for.

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// DECLARE our variables
+
+// WHEN the page opens up the site starts with a title and a start button.
+
+// WHEN the user clicks the START BUTTON we want the timer in the top-left corner to start counting down from 60.
+
+// THEN the user is presented with a question.
+
+// IF the user answers a question correctly then they are presented with another question and the user is rewarded a point.
+
+// IF NOT then we don't want the page to change the question, but notify them in a textbox below that they are wrong and 5 seconds are removed off of the timer.
+
+// WHEN all questions are answered or the timer reaches 0 then the game is over. 
+
+// WHEN the game is over then the user can save their name and score and it updates to the leaderboard.
+
