ee6c9cced035be94b33f3da01313cddbc70c9018
Taani Maama <taani1maama@live.com>
2020-10-28 22:08:09 -0700

    Add all the global variables I believe I am going to need.

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,57 @@
 // DECLARE our variables
+let startBtnEL = document.getElementsByClassName('startBtn');
+let answerBtnEL = document.getElementsByClassName('answerBtn');
+let questionEL = document.getElementsByClassName('question');
+let gameOverEL = document.getElementsByClassName('gameOverScreen')
+let submitBtnEL = document.getElementsByClassName('submit')
+let userIdEL = document.getElementById('#userInitials')
+let quizQuestions = {
 
-// WHEN the page opens up the site starts with a title and a start button.
+};
+let timer = 0;
+let score = timer;
 
+function startQuiz() {
 // WHEN the user clicks the START BUTTON we want the timer in the top-left corner to start counting down from 60.
 
-// THEN the user is presented with a question.
+};
+
+function timeLeft() {
+
+};
+
+function nextQuestion() {
+// THEN the user is presented with a question. 
 
 // IF the user answers a question correctly then they are presented with another question and the user is rewarded a point.
 
 // IF NOT then we don't want the page to change the question, but notify them in a textbox below that they are wrong and 5 seconds are removed off of the timer.
 
+
+};
+
+function gameOverScreen() {
+// WHEN the page opens up the site starts with a title and a start button.
+
+};
+
+function submitScore() {
+// WHEN the game is over then the user can save their name and score and it updates to the leaderboard.
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
 // WHEN all questions are answered or the timer reaches 0 then the game is over. 
 
-// WHEN the game is over then the user can save their name and score and it updates to the leaderboard.
+
 
