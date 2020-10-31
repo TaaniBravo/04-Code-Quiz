8229c4cf46ea99ce3da97b3c86207b5491f9fda6
Taani Maama <taani1maama@live.com>
2020-10-30 22:55:35 -0700

    I figured out how to make the answer button elements dynamic, which cuts a ton of code. I need to use forEach and datasets to make it happen.

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,36 @@
 // DECLARE our variables
 // Start Screen Variables
-let startBtnEl = document.getElementById('startBtn');
-let startScreenEl = document.getElementById('startScreen');
+const startBtnEl = document.getElementById('startBtn');
+const startScreenEl = document.getElementById('startScreen');
 
 // Question Screen Variables
-let questionEl = document.getElementById('question');
-let questionContainerEl = document.getElementById('questionContainer');
+const questionEl = document.getElementById('question');
+const questionContainerEl = document.getElementById('questionContainer');
 
-let answerBtnAEl = document.getElementById('answer1');
-let answerBtnBEl = document.getElementById('answer2');
-let answerBtnCEl = document.getElementById('answer3');
-let answerBtnDEl = document.getElementById('answer4');
+// const answerBtnAEl = document.getElementById('answer1');
+// const answerBtnBEl = document.getElementById('answer2');
+// const answerBtnCEl = document.getElementById('answer3');
+// const answerBtnDEl = document.getElementById('answer4');
 
-let correctEl = document.getElementById('correctSign');
-let wrongEl = document.getElementById('wrongSign');
-let correctWrongSectionEl = document.getElementById('correctAndWrong');
+const answers = Array.from(document.querySelectorAll('.answerBtn'));
+
+const correctEl = document.getElementById('correctSign');
+const wrongEl = document.getElementById('wrongSign');
+const correctWrongSectionEl = document.getElementById('correctAndWrong');
 
 let randomQuestion;
 let questionIndex;
 let currentQuestion;
 
 // Game Over Screen Variables
-let gameOverEl = document.getElementById('gameOverScreen');
-let submitBtnEl = document.getElementById('submitBtn');
-let retakeBtnEl = document.getElementById('retakeBtn');
-let userIdEL = document.getElementById('#userInitials');
+const gameOverEl = document.getElementById('gameOverScreen');
+const submitBtnEl = document.getElementById('submitBtn');
+const retakeBtnEl = document.getElementById('retakeBtn');
+const userIdEL = document.getElementById('#userInitials');
 
 // Timer/Score Variables
 let timeLeft = 75;
-let timerDisplayEL = document.getElementById('timer');
+const timerDisplayEL = document.getElementById('timer');
 
 // console.log(answerBtnEl)
 
@@ -74,35 +76,40 @@ function startQuiz() {
     randomQuestion = quizQuestions.sort(() => Math.random() - .5);
     
     // WHEN the question screen appears we want the first question to appear on the HTML provided.
-    showQuestion(randomQuestion, questionIndex);
+    showNewQuestion(randomQuestion, questionIndex);
     
 };
 
-function showQuestion(randomQuestion, questionIndex) {
+function showNewQuestion(randomQuestion, questionIndex) {
 
     // THEN the user is presented with a question. 
     let currentQuestion = randomQuestion[questionIndex];
     questionEl.innerText = currentQuestion.question
 
     // The innerText of each btn is set to the currentQuestions answers.
-    answerBtnAEl.innerText = 'A. ' + currentQuestion.answer1
-    answerBtnBEl.innerText = 'B. ' + currentQuestion.answer2
-    answerBtnCEl.innerText = 'C. ' + currentQuestion.answer3
-    answerBtnDEl.innerText = 'D. ' + currentQuestion.answer4
+    // answerBtnAEl.innerText = 'A. ' + currentQuestion.answer1
+    // answerBtnBEl.innerText = 'B. ' + currentQuestion.answer2
+    // answerBtnCEl.innerText = 'C. ' + currentQuestion.answer3
+    // answerBtnDEl.innerText = 'D. ' + currentQuestion.answer4
 
-    // The values are also set as we will use this to get the correctAnswer.
-    answerBtnAEl.value = currentQuestion.answer1
-    answerBtnBEl.value = currentQuestion.answer2
-    answerBtnCEl.value = currentQuestion.answer3
-    answerBtnDEl.value = currentQuestion.answer4
+    // // The values are also set as we will use this to get the correctAnswer.
+    // answerBtnAEl.value = currentQuestion.answer1
+    // answerBtnBEl.value = currentQuestion.answer2
+    // answerBtnCEl.value = currentQuestion.answer3
+    // answerBtnDEl.value = currentQuestion.answer4
+
+    answers.forEach(answer => {
+        const number = answer.dataset['number'];
+        answer.innerText = currentQuestion['answer' + number];
+    })
 
     let correctAnswer = currentQuestion.correctAnswer
 
-    console.log(correctAnswer)
-    console.log(answerBtnAEl.value)
-    console.log(answerBtnBEl.value)
-    console.log(answerBtnCEl.value)
-    console.log(answerBtnDEl.value)
+    // console.log(correctAnswer)
+    // console.log(answerBtnAEl.value)
+    // console.log(answerBtnBEl.value)
+    // console.log(answerBtnCEl.value)
+    // console.log(answerBtnDEl.value)
 
     handleAnswerClick(correctAnswer)
 
@@ -158,7 +165,7 @@ function rightAnswer() {
     correctWrongSectionEl.classList.remove('hide')
     correctEl.classList.remove('hide')
 
-    setInterval(() => {
+    setTimeout(() => {
         // THEN ADD the hide class again so that the elements don't stay on the page after 750ms.
         correctWrongSectionEl.classList.add('hide')
         correctEl.classList.add('hide')
@@ -167,7 +174,7 @@ function rightAnswer() {
     resetValue();
 
     // run show question again
-    setInterval(showQuestion(randomQuestion, questionIndex), 1000);
+    setTimeout(showNewQuestion(randomQuestion, questionIndex), 1000);
 }
 
 function resetValue() {
@@ -187,7 +194,7 @@ function wrongAnswer() {
     correctWrongSectionEl.classList.remove('hide')
     wrongEl.classList.remove('hide')
 
-    setInterval(() => {
+    setTimeout(() => {
         // THEN ADD the hide class again so that the elements don't stay on the page after 750ms.
         correctWrongSectionEl.classList.add('hide')
         wrongEl.classList.add('hide')
@@ -207,7 +214,7 @@ let quizQuestions = [
         answer2: 'Tingle',
         answer3: 'Skull Kid',
         answer4: 'Link',
-        correctAnswer: 'Link',
+        correctAnswer: 4,
     },
     {
         question: 'What is the TriForce?',
@@ -215,7 +222,7 @@ let quizQuestions = [
         answer2: 'Tattoo for members of the Yiga Clan',
         answer3: 'The instrument Link plays music with',
         answer4: 'A powerful finishing move',
-        correctAnswer: 'The power of the Golden Goddesses',
+        correctAnswer: 1,
     },
     {
         question: 'Who is the usual main antagonist?',
@@ -223,7 +230,7 @@ let quizQuestions = [
         answer2: 'King of Red Lions',
         answer3: 'Majora',
         answer4: 'Ganon',
-        correctAnswer: 'Ganon',
+        correctAnswer: 4,
     },
     {
         question: 'What does each piece of the TriForce stand for?',
@@ -231,7 +238,7 @@ let quizQuestions = [
         answer2: 'Water, Fire, and Grass',
         answer3: 'Power, Wisdom, and Courage',
         answer4: 'Love, Peace, and Tranquility',
-        correctAnswer: 'Power, Wisdom, and Courage',
+        correctAnswer: 3,
     },
     {
         question: 'Who created the Legend of Zelda?',
@@ -239,7 +246,7 @@ let quizQuestions = [
         answer2: 'Takashi Tezuke',
         answer3: 'Shigeki Morimoto',
         answer4: 'Koji Okada',
-        correctAnswer: 'Shigeru Miyamoto',
+        correctAnswer: 1,
     },
     {
         question: 'What is Zelda\'s alter ego in Ocarina of Time?',
@@ -247,7 +254,7 @@ let quizQuestions = [
         answer2: 'Sheik',
         answer3: 'Tetra',
         answer4: 'Saria',
-        correctAnswer: 'Sheik',
+        correctAnswer: 2,
     },
     {
         question: 'When was the first Legend of Zelda originally released?',
@@ -255,7 +262,7 @@ let quizQuestions = [
         answer2: '1982',
         answer3: '1985',
         answer4: '1990',
-        correctAnswer: '1986',
+        correctAnswer: 1,
     },
     {
         question: 'Which of the games is set in the span of 3 days?',
@@ -263,7 +270,7 @@ let quizQuestions = [
         answer2: 'Link\s Awakening',
         answer3: 'Majora\'s Mask',
         answer4: 'Minish Cap',
-        correctAnswer: 'Majora\'s Mask',
+        correctAnswer: 3,
     },
     {
         question: 'Which of the games main plot involves time travel?',
@@ -271,7 +278,7 @@ let quizQuestions = [
         answer2: 'Twilight Princess',
         answer3: 'The Adventure of Link',
         answer4: 'Ocarina of Time',
-        correctAnswer:'Ocarina of Time',
+        correctAnswer:4,
     },
     {
         question: 'What is the best selling Legend of Zelda game?',
@@ -279,7 +286,7 @@ let quizQuestions = [
         answer2: 'Legend of Zelda (the Original)',
         answer3: 'Breath of the Wild',
         answer4: 'Phantom Hourglass',
-        correctAnswer: 'Breath of the Wild',
+        correctAnswer: 3,
     }
 ];
 
